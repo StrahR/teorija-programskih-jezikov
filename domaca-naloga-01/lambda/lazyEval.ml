a5918fb67b2e4aa131c78240cf977a8105146039
Rok Strah <muf@localhost.localdomain>
2019-11-22 20:32:15 +0100

    fixed lazy eval

--- domaca-naloga-01/lambda/lazyEval.ml
+++ domaca-naloga-01/lambda/lazyEval.ml
@@ -43,24 +43,20 @@ let rec eval_exp = function
       | _ -> failwith "Function expected"
     end
   | S.Nil -> S.Nil
-  | S.Pair (e1, e2) -> S.Pair (eval_exp e1, eval_exp e2)
-  | S.Fst e ->
-    begin match e with
-      | S.Pair (e1, e2) -> eval_exp e1
-      | _ -> S.Fst (eval_exp e)
-    end
-  | S.Snd e ->
-    begin match e with
-      | S.Pair (e1, e2) -> eval_exp e2
-      | _ -> S.Snd (eval_exp e)
-    end
-  | S.Cons (e, es) -> S.Cons (eval_exp e, eval_exp es)
-  | S.Match (e, e1, x, xs, e2) ->
-    begin match e with
-      | S.Nil -> eval_exp e1
-      | S.Cons (v, vs) -> eval_exp (S.subst [(x, v); (xs, vs)] e2)
-      | _ -> eval_exp (S.Match (eval_exp e, e1, x, xs, e2))
-    end
+  | S.Pair _ as p -> p
+  | S.Fst S.Pair (e, _) -> eval_exp e
+  | S.Fst S.Cons (e, _) -> eval_exp e
+  | S.Fst e -> S.Fst (eval_exp e)
+  | S.Snd S.Pair (_, e) -> eval_exp e
+  | S.Snd S.Cons (_, e) -> eval_exp e
+  | S.Snd e -> S.Snd (eval_exp e)
+  | S.Cons _ as l -> l
+  | S.Match (S.Nil, e1, _, _, _) -> eval_exp e1
+  | S.Match (S.Cons (e, es), _, x, xs, e2) ->
+    let v = eval_exp e
+    and vs = eval_exp es
+    in eval_exp (S.subst [(x, v); (xs, vs)] e2)
+  | S.Match (e, e1, x, xs, e2) -> S.Match (eval_exp e, e1, x, xs, e2)
 
 and eval_int e =
   match eval_exp e with
@@ -102,16 +98,11 @@ let rec step = function
   | S.Apply (S.RecLambda (f, x, e) as rec_f, e2)-> S.subst [(f, rec_f); (x, e2)] e
   | S.Apply (e1, e2) -> S.Apply (step e1, e2)
   | S.Nil -> S.Nil
-  | S.Pair (v1, v2) as p when is_value v1 && is_value v2 -> p
-  | S.Pair (v1, e2) when is_value v1 -> S.Pair (v1, step e2)
-  | S.Pair (e1, e2) -> S.Pair (step e1, e2)
-  | S.Fst S.Pair (e1, e2) -> e1
-  | S.Fst _ -> failwith "Pair Expected"
-  | S.Snd S.Pair (e1, e2) -> e2
-  | S.Snd _ -> failwith "Pair Expected"
-  | S.Cons (v, vs) as l when is_value v && is_value vs -> l
-  | S.Cons (v, xs) when is_value v -> S.Cons (v, step xs)
-  | S.Cons (x, xs) -> S.Cons (step x, xs)
+  | S.Pair _ as p -> p
+  | S.Fst S.Pair (e, _) | S.Snd S.Pair (_, e)
+  | S.Fst S.Cons (e, _) | S.Snd S.Cons (_, e) -> e
+  | S.Fst _ | S.Snd _ -> failwith "Pair or Cons Expected"
+  | S.Cons _ as l -> l
   | S.Match (S.Nil, e1, x, xs, e2) -> e1
   | S.Match (S.Cons (v, vs) as e, e1, x, xs, e2) when is_value e ->
     (S.subst [(x, v); (xs, vs)] e2)
